321538eb5ae31680e78dec23a428965f987b2876
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-01-25 16:34:30 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -29,23 +29,3 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
-
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
-{
-    if ( computedStats > maxThreshold)
-    {
-        //change to for loop with size of
-        alerters[0];
-        alerters[1];
-    }
-}
-
-void emailAlerter ()
-{
-    emailAlertCallCount = 1;
-}   
-
-void ledAlerter () 
-{
-    ledAlertCallCount = 1;
-}
