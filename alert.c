442a7f4ca93f8d7a1fd245961ae92065c0dfd4c7
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-01-25 16:58:29 +0530

    Update alert.c

--- alert.c
+++ alert.c
@@ -1,4 +1,26 @@
 #include "stats.h"
-alerter_funcptr emailAlerter();
+#include "alert.h"
 
-alerter_funcptr ledAlerter();
+
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    if ( computedStats > maxThreshold)
+    {
+        //change to for loop with size of
+        alerters[0];
+        alerters[1];
+    }
+}
+
+void emailAlerter ()
+{
+    emailAlertCallCount = 1;
+}   
+
+void ledAlerter () 
+{
+    ledAlertCallCount = 1;
+}
