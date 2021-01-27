e53350ae27774a57a859cb2904c33b1be66853f0
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-01-27 09:09:44 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,28 +1,65 @@
-  
-#include "stats.h"
+#include "alerter.h"
+
+
+void swap(float *xp, float *yp)
+{
+    float temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
-    s.min = numberset[0];
-    s.max =  numberset[0];
-    float sum = 0;
-    
-    for(int i = 0 ; i < setlength ;  i++ )
+    s.min = 0;
+    s.max = 0;
+    int next_index=0;
+    int i,j,k;
+	float sum=0.0;
+	float lnumberset[setlength-1];
+
+	for (int loop_counter=0; loop_counter<setlength;loop_counter++)
+	{
+		lnumberset[loop_counter]=numberset[loop_counter];
+	}
+	for (k=0;k<setlength;k++)
     {
-        sum += numberset[i];
-        /*To calculate minimim of array*/
-        if ( numberset[i] < s.min)
-        {
-            s.min =  numberset[i];
-        }
-         /*To calculate maximum of array*/
-        if ( numberset[i] > s.max)
-        {
-            s.max =  numberset[i];
-        }
+        sum = sum + lnumberset[k];
     }
-    s.average = sum / setlength ;
-  
-    return (s);  
+
+    s.average= (float)sum / setlength ;
+	
+    for (i=0;i<setlength-1;i++)
+    {
+        next_index=i;
+        for (j=i+1;j<setlength;j++)
+        {
+            if (lnumberset[j]<lnumberset[next_index])
+            {
+                next_index=j;
+            }
+        }
+
+        swap(&lnumberset[next_index],&lnumberset[i]);
+    }
+
+    s.max=lnumberset[setlength-1];
+    s.min=lnumberset[0];
+
+    return s;
+}
+
+
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+
+void check_and_alert(const float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    if(computedStats.max>maxThreshold)
+    {
+      alerters[0]();
+      alerters[1]();
+
+    }
 }
